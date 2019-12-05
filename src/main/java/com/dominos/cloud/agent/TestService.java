50ccf73ac9841e8040bbc06c1379332dde5b6b17
zhangzengmin <xiaomin0322@sina.com>
2019-12-05 16:22:27 +0800

    update

--- src/main/java/com/dominos/cloud/agent/TestService.java
+++ src/main/java/com/dominos/cloud/agent/TestService.java
@@ -2,8 +2,11 @@ package com.dominos.cloud.agent;
 
 public class TestService {
 
-	
 	public static void main(String[] args) {
+		new TestService().print();
+	}
+
+	public void print() {
 		System.out.println("zzm");
 	}
 }
