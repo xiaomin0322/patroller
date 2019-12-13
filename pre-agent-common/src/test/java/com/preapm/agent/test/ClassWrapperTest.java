b399326bb288eb048f96debe5472d25909f92498
zhangzengmin <xiaomin0322@sina.com>
2019-12-13 10:16:46 +0800

    update

---
+++ pre-agent-common/src/test/java/com/preapm/agent/test/ClassWrapperTest.java
@@ -0,0 +1,22 @@
+package com.preapm.agent.test;
+
+import com.preapm.agent.common.weave.ClassWrapper;
+
+public class ClassWrapperTest {
+
+	
+	public static void main(String[] args) {
+		String test = "12313";
+
+		String s = " System.out.println(\"加入span成功：\"+" + ClassWrapper.toStr(test) + ");\r\n";
+		
+		System.out.println(s);
+		
+		s = " System.out.println(\"参数名称:\"+"+ ClassWrapper.toStrto(test)
+		 +"\"参数值：\"+"+test+");\r\n";
+		
+		System.out.println(s);
+		
+
+	}
+}
