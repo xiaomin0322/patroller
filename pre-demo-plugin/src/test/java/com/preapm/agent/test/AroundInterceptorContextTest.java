bb4db10c67f47cfca9cd7f7d63bc57ed205633cb
zhangzengmin <xiaomin0322@sina.com>
2019-12-13 11:57:17 +0800

    update

---
+++ pre-demo-plugin/src/test/java/com/preapm/agent/test/AroundInterceptorContextTest.java
@@ -0,0 +1,12 @@
+package com.preapm.agent.test;
+
+import com.preapm.agent.common.context.AroundInterceptorContext;
+
+public class AroundInterceptorContextTest {
+
+	public static void main(String[] args) {
+		AroundInterceptorContext.start(null, null);
+
+	}
+
+}
