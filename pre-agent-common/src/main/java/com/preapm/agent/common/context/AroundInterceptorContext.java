2f119e652300c6e45552eaee6304b589598e46e2
zhangzengmin <xiaomin0322@sina.com>
2019-12-17 16:59:50 +0800

    123

--- pre-agent-common/src/main/java/com/preapm/agent/common/context/AroundInterceptorContext.java
+++ pre-agent-common/src/main/java/com/preapm/agent/common/context/AroundInterceptorContext.java
@@ -1,9 +1,12 @@
 package com.preapm.agent.common.context;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.ServiceLoader;
 import java.util.Set;
 
@@ -15,12 +18,17 @@ public class AroundInterceptorContext {
 	public static String agentId = "test";
 
 	public static Set<AroundInterceptor> interceptors = new HashSet<AroundInterceptor>();
-	
-	
-	public static Map<String,AroundInterceptor> interceptorsMap = new HashMap<String,AroundInterceptor>();
+
+	public static Map<String, AroundInterceptor> interceptorsMap = new HashMap<String, AroundInterceptor>();
 
 	public static void start(MethodInfo methodInfo) {
 		for (AroundInterceptor i : interceptors) {
+			i.before(methodInfo);
+		}
+	}
+
+	public static void start(MethodInfo methodInfo, Set<String> names) {
+		for (AroundInterceptor i : get(names)) {
 			i.before(methodInfo);
 		}
 	}
@@ -39,6 +47,29 @@ public class AroundInterceptorContext {
 		for (AroundInterceptor i : interceptors) {
 			i.exception(methodInfo);
 		}
+	}
+
+	public static void after(MethodInfo methodInfo, Set<String> names) {
+		for (AroundInterceptor i : get(names)) {
+			i.after(methodInfo);
+		}
+	}
+
+	public static void exception(MethodInfo methodInfo, Set<String> names) {
+		for (AroundInterceptor i : get(names)) {
+			i.exception(methodInfo);
+		}
+	}
+
+	public static List<AroundInterceptor> get(Set<String> names) {
+		List<AroundInterceptor> list = new ArrayList<AroundInterceptor>();
+		for (Entry<String, AroundInterceptor> e : interceptorsMap.entrySet()) {
+			String name = e.getKey();
+			if (names.contains(name)) {
+				list.add(e.getValue());
+			}
+		}
+		return list;
 	}
 
 	public static void init() {
