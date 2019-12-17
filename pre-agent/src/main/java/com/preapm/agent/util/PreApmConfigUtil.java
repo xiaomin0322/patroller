ff191bea90c01a553397e0cd48ec1f4d2fa228f6
zhangzengmin <xiaomin0322@sina.com>
2019-12-17 19:45:15 +0800

    12123

--- pre-agent/src/main/java/com/preapm/agent/util/PreApmConfigUtil.java
+++ pre-agent/src/main/java/com/preapm/agent/util/PreApmConfigUtil.java
@@ -85,7 +85,7 @@ public class PreApmConfigUtil {
 		bean.setPatterns("org.apache.http.impl.client.CloseableHttpClient");
 		bean.setContainPatterns(methodSet);
 		methodSet = new HashSet<>();
-		methodSet.add("pre-HttpClient4-plugin");
+		methodSet.add("pre-httpClient4-plugin");
 		bean.setPlugins(methodSet);
 		targetMap.put(bean.getPatterns(), bean);
 
