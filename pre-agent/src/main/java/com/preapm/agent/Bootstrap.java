8b37e7f5651d89ac32078d7f4779d82ecd59709e
zhangzengmin <xiaomin0322@sina.com>
2019-12-17 19:23:11 +0800

    123

--- pre-agent/src/main/java/com/preapm/agent/Bootstrap.java
+++ pre-agent/src/main/java/com/preapm/agent/Bootstrap.java
@@ -3,22 +3,22 @@ package com.preapm.agent;
 public class Bootstrap {
 
 	public static void main(String[] args) {
-		new Bootstrap().print("123123");
+		new Bootstrap().print2("123123");
 		new Bootstrap().print();
-		new Bootstrap().print("123123","123");
+		new Bootstrap().print3("123123","123");
 	}
 
 	public void print() {
 		System.out.println("zzm");
 	}
 
-	public String print(String s) {
+	public String print2(String s) {
 		System.out.println("zzm " + s);
 		return s;
 	}
-	public String print(String s,String s2) {
+	public String print3(String s,String s2) {
 		System.out.println("zzm " + s);
-		print(s2);
+		print2(s2);
 		throw new RuntimeException("test");
 	}
 	
