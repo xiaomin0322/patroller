2d05ff8945a0796a618d6d9f54b416114804da95
zhangzengmin <xiaomin0322@sina.com>
2019-12-19 14:23:07 +0800

    12312321322222222222222222

--- pre/pre-httpclient4-plugin/src/main/java/com/preapm/agent/plugin/interceptor/filter/HttpResponseFilter.java
+++ pre/pre-httpclient4-plugin/src/main/java/com/preapm/agent/plugin/interceptor/filter/HttpResponseFilter.java
@@ -5,20 +5,22 @@ import java.io.IOException;
 import org.apache.http.HttpException;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.protocol.HttpContext;
+
 
 /**
  * org.apache.http.impl.client.HttpClientBuilder.HttpClientBuilder()
- * 
  * @author Zengmin.Zhang
  *
  */
-public class HttpRequestFilter implements HttpRequestInterceptor {
+public class HttpResponseFilter implements HttpResponseInterceptor{
 
 	@Override
-	public void process(HttpRequest request, HttpContext context) throws HttpException, IOException {
-		System.out.println(request.getRequestLine().getUri());
-
+	public void process(HttpResponse response, HttpContext context) throws HttpException, IOException {
+		System.out.println("end>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 	}
+
 
 }
---
+++ pre/pre-httpclient4-plugin/src/test/java/com/preapm/agent/test/httpclient/HttpClientTest.java
@@ -0,0 +1,10 @@
+package com.preapm.agent.test.httpclient;
+
+public class HttpClientTest {
+
+	
+	public static void main(String[] args) {
+		HttpClientUtil.doGet("https://www.cnblogs.com/Eric-zhao/p/11679643.html");
+		
+	}
+}
