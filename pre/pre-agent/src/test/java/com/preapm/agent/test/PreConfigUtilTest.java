787871b4e52777599c7a9c7c2bc434fe8a82b2c8
zhangzengmin <xiaomin0322@sina.com>
2019-12-23 15:25:11 +0800

    123123

---
+++ pre/pre-agent/src/test/java/com/preapm/agent/test/PreConfigUtilTest.java
@@ -0,0 +1,13 @@
+package com.preapm.agent.test;
+
+import com.preapm.agent.util.PreConfigUtil;
+
+public class PreConfigUtilTest {
+	
+	
+	public static void main(String[] args) {
+		System.out.println(PreConfigUtil.isTarget("com.preapm.agent.Bootstrap"));
+		System.out.println(PreConfigUtil.isTarget("com.preapm.agent.Bootstrap", "com.preapm.agent.Bootstrap\\(\\)"));
+	}
+
+}
