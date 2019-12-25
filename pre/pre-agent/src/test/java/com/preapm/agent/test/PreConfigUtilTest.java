7c1655d7794a51f91cb63a32f2d6a8adb353b1e5
zhangzengmin <xiaomin0322@sina.com>
2019-12-25 15:36:05 +0800

    123

--- pre/pre-agent/src/test/java/com/preapm/agent/test/PreConfigUtilTest.java
+++ pre/pre-agent/src/test/java/com/preapm/agent/test/PreConfigUtilTest.java
@@ -1,16 +1,30 @@
 package com.preapm.agent.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.preapm.agent.util.PreConfigUtil;
+import com.preapm.agent.weave.pattern.JdkRegexpMethodPointcut;
 
 public class PreConfigUtilTest {
-	
-	
+
 	public static void main(String[] args) {
-		System.out.println(PreConfigUtil.isTargetR("com.preapm.agent.Bootstrap", "com.preapm.agent.Bootstrap()"));
-		System.out.println(PreConfigUtil.isTarget("com.preapm.agent.Bootstrap", "com.preapm.agent.Bootstrap.print2(java.lang.String)"));
-        
-		System.out.println(PreConfigUtil.isTargetR("com.dominos.cloud.im.controller.StoreController", "test()"));
-	
+		/*
+		 * System.out.println(PreConfigUtil.isTargetR("com.preapm.agent.Bootstrap",
+		 * "com.preapm.agent.Bootstrap()"));
+		 * System.out.println(PreConfigUtil.isTarget("com.preapm.agent.Bootstrap",
+		 * "com.preapm.agent.Bootstrap.print2(java.lang.String)"));
+		 * System.out.println(PreConfigUtil.isTargetR(
+		 * "com.dominos.cloud.im.controller.StoreController", "test()"));
+		 */
+
+		System.out.println(
+				PreConfigUtil.isTargetR("redis.clients.jedis.BinaryJedis", "redis.clients.jedis.BinaryJedis.ping()"));
+
+		List<String> patternsList = new ArrayList<>();
+		patternsList.add("redis.clients.jedis.BinaryJedis.ping.*");
+
+		System.out.println(JdkRegexpMethodPointcut.macth(patternsList, "redis.clients.jedis.BinaryJedis.ping()"));
 	}
 
 }
