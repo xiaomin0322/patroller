122c1767b4ef2845f98b9cdfe2769f8494a5557f
zhangzengmin <xiaomin0322@sina.com>
2019-12-25 14:01:24 +0800

    123

--- pre/pre-mysql-jdbc-driver-plugin/src/main/java/com/preapm/agent/plugin/interceptor/filter/MySQLStatementInterceptor.java
+++ pre/pre-mysql-jdbc-driver-plugin/src/main/java/com/preapm/agent/plugin/interceptor/filter/MySQLStatementInterceptor.java
@@ -3,39 +3,37 @@ package com.preapm.agent.plugin.interceptor.filter;
 import java.sql.SQLException;
 import java.util.Properties;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.ResultSetInternalMethods;
 import com.mysql.jdbc.Statement;
 import com.mysql.jdbc.StatementInterceptorV2;
+import com.preapm.sdk.zipkin.ZipkinClient;
+import com.preapm.sdk.zipkin.ZipkinClientContext;
 
 public class MySQLStatementInterceptor implements StatementInterceptorV2 {
-	private static final Logger logger = LoggerFactory.getLogger(MySQLStatementInterceptor.class);
+	public static final String SQL_STR = "SQL";
 
 	@Override
 	public void init(Connection conn, Properties props) throws SQLException {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public ResultSetInternalMethods preProcess(String sql, Statement interceptedStatement, Connection connection)
 			throws SQLException {
-		// TODO Auto-generated method stub
+		ZipkinClient client = ZipkinClientContext.getClient();
+		client.startSpan(SQL_STR);
+		client.sendBinaryAnnotation(SQL_STR, sql);
 		return null;
 	}
 
 	@Override
 	public boolean executeTopLevelOnly() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
 
 	}
 
@@ -43,7 +41,7 @@ public class MySQLStatementInterceptor implements StatementInterceptorV2 {
 	public ResultSetInternalMethods postProcess(String sql, Statement interceptedStatement,
 			ResultSetInternalMethods originalResultSet, Connection connection, int warningCount, boolean noIndexUsed,
 			boolean noGoodIndexUsed, SQLException statementException) throws SQLException {
-		// TODO Auto-generated method stub
+		ZipkinClientContext.getClient().finishSpan();
 		return null;
 	}
 
