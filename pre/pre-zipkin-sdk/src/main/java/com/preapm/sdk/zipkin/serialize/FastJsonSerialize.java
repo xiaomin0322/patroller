1744a0af8acb4adcefb5ac534ea3aa9e2b3dfe01
zhangzengmin <xiaomin0322@sina.com>
2019-12-30 20:36:06 +0800

    123

--- pre/pre-zipkin-sdk/src/main/java/com/preapm/sdk/zipkin/serialize/FastJsonSerialize.java
+++ pre/pre-zipkin-sdk/src/main/java/com/preapm/sdk/zipkin/serialize/FastJsonSerialize.java
@@ -1,5 +1,9 @@
 package com.preapm.sdk.zipkin.serialize;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+
 import com.alibaba.fastjson.JSONObject;
 
 public class FastJsonSerialize implements SerializeInterface{
@@ -14,6 +18,9 @@ public class FastJsonSerialize implements SerializeInterface{
 		String className = object.getClass().getName();
 		if("javax.servlet.ServletRequest".equals(className) || "javax.servlet.ServletResponse".equals(className) ||
 				"org.springframework.web.multipart.MultipartFile".equals(className)) {
+			return className;
+		}
+		if (object instanceof ServletRequest || object instanceof ServletResponse) {
 			return className;
 		}
 		return JSONObject.toJSONString(object);
