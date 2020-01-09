d0a6891b89d8b7abb297f7ece33ed9cc23d3c0a8
zhangzengmin <xiaomin0322@sina.com>
2020-01-09 14:56:12 +0800

    log4j

--- pre/pre-log4j-plugin/src/main/java/com/preapm/agent/plugin/interceptor/Log4jInterceptor.java
+++ pre/pre-log4j-plugin/src/main/java/com/preapm/agent/plugin/interceptor/Log4jInterceptor.java
@@ -2,16 +2,16 @@ package com.preapm.agent.plugin.interceptor;
 
 import com.preapm.agent.common.bean.MethodInfo;
 import com.preapm.agent.common.interceptor.AroundInterceptor;
+import com.preapm.agent.plugin.interceptor.filter.PreLog4JFilter;
 
 /**
  * 
+ * org.apache.log4j.PropertyConfigurator.parseAppender(Properties, String)
  * 
  * @author Zengmin.Zhang
  *
  */
 public class Log4jInterceptor implements AroundInterceptor {
-
-	public static String PACKAGENAME = "com.preapm.agent.plugin.interceptor.filter";
 
 	@Override
 	public void before(MethodInfo methodInfo) {
@@ -24,6 +24,10 @@ public class Log4jInterceptor implements AroundInterceptor {
 
 	@Override
 	public void after(MethodInfo methodInfo) {
+		org.apache.log4j.Appender appender = (org.apache.log4j.Appender) methodInfo.getResult();
+		if (appender != null) {
+			appender.addFilter(new PreLog4JFilter());
+		}
 	}
 
 	@Override
