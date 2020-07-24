d3c67c7e47567a40638cece352c297c2e29097c1
zhangzengmin <xiaomin0322@sina.com>
2020-07-24 17:46:42 +0800

    update

--- pre/pre-jdk-thread-plugin/src/main/java/com/preapm/agent/plugin/interceptor/JdkThreadClassInterceptor.java
+++ pre/pre-jdk-thread-plugin/src/main/java/com/preapm/agent/plugin/interceptor/JdkThreadClassInterceptor.java
@@ -24,13 +24,13 @@ public class JdkThreadClassInterceptor implements ClassInterceptor {
 					ctClass);
 			ctClass.addField(f);
 			
-			//也可在外面通用拦截器配置
-		/*	CtMethod declaredMethod = ctClass.getDeclaredMethod("run");
+			//在此处植入拦截器比较合适，因为这是一个逻辑，如果分开在外面配置拦截器，有可能不同的线程在不同的包中，比较分散，导致配置的不全。会出现问题
+			CtMethod declaredMethod = ctClass.getDeclaredMethod("run");
 			if(declaredMethod == null) {
 				declaredMethod = ctClass.getDeclaredMethod("call");
 			}
 			declaredMethod.insertBefore("com.preapm.sdk.zipkin.ZipkinClientContext.getClient().getSpanStore().setSpan(span.toBuilder());");
-			declaredMethod.insertAfter("com.preapm.sdk.zipkin.ZipkinClientContext.getClient().getSpanStore().removeSpan();");*/
+			declaredMethod.insertAfter("com.preapm.sdk.zipkin.ZipkinClientContext.getClient().getSpanStore().removeSpan();");
 	  } catch (Exception e) {
 			e.printStackTrace();
 		}
