b905d9a207375fe98f41dfe1b08be9167f2f4b01
skonline <517672879@qq.com>
2022-05-17 19:59:32 +0800

    Update ThreadLocalSpanStore.java

--- pre/pre-zipkin-sdk/src/main/java/com/preapm/sdk/zipkin/ThreadLocalSpanStore.java
+++ pre/pre-zipkin-sdk/src/main/java/com/preapm/sdk/zipkin/ThreadLocalSpanStore.java
@@ -12,6 +12,10 @@ import zipkin.Span;
  * ThreadLocalSpanStore,LazyTraceExecutor
  * </pre>
  * 
+ * span跨线程传递数据的时候，需要获取父线程的数据，拷贝到子线程中。
+ * JDK自带的InheritableThreadLocal仅能解决每次手动创建线程的场景，如果使用线程池，它将无能为力。
+ * 阿里开源的TTL仅能解决线程池的场景，手动创建线程无法解决。
+ * 因此，采用自定义thread plugin的方式传递数据
  * @author 
  *
  * @since 2018年1月25日 下午4:18:02
